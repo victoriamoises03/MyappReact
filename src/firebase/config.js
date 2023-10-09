e3e25d2e7946cbadd44ce33d81c4cd8f95fea156
victoriamoises03 <vmoises@udesa.edu.ar>
2023-10-09 11:24:16 -0300

    si

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+import app from "firebase/app";
+import firebase from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA5O-LnUcNz8mWj2p1OYZ1KC87Qs5f3iMc",
+  authDomain: "proyectofinalp3-aa4fb.firebaseapp.com",
+  projectId: "proyectofinalp3-aa4fb",
+  storageBucket: "proyectofinalp3-aa4fb.appspot.com",
+  messagingSenderId: "817599454772",
+  appId: "1:817599454772:web:cdb806be4c398fa19af461"
+};
+
+
+export const auth =
+export const  =
+export const  =
